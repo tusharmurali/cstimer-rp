2e4367b92ad99e44f98cd355c90fd4934c195b01
TusharDevelops <tushardevelops@gmail.com>
2020-08-07 01:40:51 -0400

    No times bug fix
    Fixed bug where presence would not appear when no times were registered in the session

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import time
 client_id = '723966115537223721'  # Bot ID
 RPC = Presence(client_id)  # Initialize the client class
 RPC.connect()  # Start the handshake loop
-
 print("csTimer Rich Presence connected")
 
 tooltips = {
@@ -28,10 +27,15 @@ tooltips = {
     "3x3mbld": "3x3x3 Multi-Blind"
 }
 
+
+def get(index):
+    return data[index] if index < len(data) - 3 else "N/A"
+
+
 while True:  # The presence will stay on as long as the program is running
     data = GetWindowText(GetForegroundWindow()).split()
     if data[0] == "csTimer":
-        RPC.update(state="ao5: " + data[10] + ", ao12: " + data[11], details="time: " + data[8] + ", mo3: " + data[9],
-                   large_image=data[7].replace(" ", ""), large_text=tooltips[data[7]],
+        RPC.update(state="ao5: " + get(10) + ", ao12: " + get(11), details="time: " + get(8) + ", mo3: " + get(9),
+                   large_image=get(7).replace(" ", ""), large_text=tooltips[get(7)],
                    small_image="cstimer", small_text="csTimer")
     time.sleep(5)  # Update every 5 seconds
